0003d3e4cb1220726fd38e06f4a73def8e926952
Lofp34 <136515564+Lofp34@users.noreply.github.com>
2026-08-07 00:53:04 +0200

    Bump article loaders for Agent Plugins deck

--- first317-folders.js
+++ first317-folders.js
@@ -53,9 +53,9 @@
     setupBackButton();
     Promise.all([
       loadScript('poetry-deck.js?v=1'),
-      loadScript('articles-study-core.js?v=1')
+      loadScript('articles-study-core.js?v=2')
     ])
-      .then(()=>loadScript('article-agent-plugins.js?v=1'))
+      .then(()=>loadScript('article-agent-plugins.js?v=2'))
       .catch(error=>console.warn('Chargement des extensions incomplet :',error));
   };
 
